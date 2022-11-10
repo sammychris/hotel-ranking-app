29f9a2b47f8125e7604d355b0b7a521a209535a2
Samuel <ebusameric@gmail.com>
2022-11-10 14:54:40 +0100

    feature: create card component

---
+++ src/components/Card/Card.styled.ts
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { brandFieldsProps, hotelFieldsProps } from '../../reducers';
+
+export interface CardProps {
+  hotel: hotelFieldsProps
+  brands: Array<brandFieldsProps>
+  onViewHotel: Function
+}
+
+
+export const Card = styled.div`
+  padding: 14px;
+  height: 510px;
+  margin: 40px auto;
+  &:hover {
+    box-shadow: 0px 2px 5px 0px #c7c7c7;
+  }
+  position: relative;
+  cursor: pointer;
+`;
+
+
+export const ProductImg = styled.div`
+  display: block;
+  width: 350px;
+  height: 300px;
+  background-size: contain;
+  position: relative;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Overlay = styled.div`
+  background: #fffefe;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0.3;
+`;
+
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import { Rate } from 'antd';
+import * as S from './Card.styled';
+import Typography from '../Typography';
+
+const Card: FC<S.CardProps> = ({hotel, brands, onViewHotel}) => {
+  const brand = brands.find(b => b.id === hotel.brandId);
+
+  return (
+    <S.Card onClick={() => onViewHotel(hotel)}>
+      <S.ProductImg style={{backgroundImage: 'url(https://th.bing.com/th/id/OIP.0vUoFeFRl_RD9_o33r0b2wHaHa?pid=ImgDet&rs=1)'}}>
+      </S.ProductImg>
+      <Typography variant='h5' style={{padding: 5}}>{hotel?.name}</Typography>
+      <Typography variant='p' style={{padding: 5}}>{hotel?.city}, {hotel?.country}</Typography>
+      <Typography variant='p' style={{padding: 5}}>{hotel?.address}</Typography> 
+      <Typography variant='p' style={{padding: 5}}>Brand: {brand?.name}</Typography>
+      <Rate allowHalf disabled defaultValue={Number(hotel?.rating)/2} />
+    </S.Card>
+  );
+}
+
+export default Card;
